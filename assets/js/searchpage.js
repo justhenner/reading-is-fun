9b9676b48a0458fbafd0232553eb56d84d2d5040
JCSH <joshmatsbux@gmail.com>
2023-01-13 18:54:04 -1000

    added content html file

--- assets/js/searchpage.js
+++ assets/js/searchpage.js
@@ -3,3 +3,7 @@ var authorTextEl = u('#author')
 var searchResultsEl = u('#searchQs')
 var searchFormEl = u('#searchForm')
 var searchBtnEl = u('#searchBtn')
+
+function getSearch() {
+    var searchElArr = u('searc')
+}
