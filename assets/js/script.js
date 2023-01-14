a1048fe046564075740fdd5844587bfa7ec885bc
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-01-13 16:48:54 -0800

    add validation for search query

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@ var searchModal = u("#search-modal");
 const searchButton = document.querySelector('#search-btn');
 const modalBg = document.querySelector('.modal-background');
 const closeButton = document.querySelector('#close-btn');
+var titleInput = document.querySelector(".title-input");
+var authorInput = document.querySelector(".author-input");
+var subjectInput = document.querySelector(".subject-input");
+var googleURL;
 
 
 
@@ -11,29 +15,52 @@ function closeSearch() {
     searchModal.removeClass("is-active");
 }
 
+// function to validate input
+function validateSearch(title, author, subject) {
+    if (!title && !author && !subject) {
+        u(".validation").append("<div class='modal is-active' id='invalid-search'><div class='modal-background close-warning'></div><div class='modal-card'><header class='modal-card-head has-background-primary'><p class='modal-card-title has-text-white'>Invalid Search</p><button class='delete close-warning' aria-label='close'></button></header><section class='modal-card-body'><p>Please enter at least one search term.</p></section><footer class='modal-card-foot'><button class='button is-primary close-warning'>OK</button></footer></div></div>");
+
+        u(".close-warning").on("click", closeValidation);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function closeValidation() {
+    u("#invalid-search").removeClass("is-active");
+}
+
+// function to build Google API URL
+
 // Search event handler
-function findBooks(event){
+function findBooks(event) {
     event.preventDefault();
 
-    console.log(u(".title-input"));
+    var title = titleInput.value.trim();
+    var author = authorInput.value.trim();
+    var subject = subjectInput.value.trim();
 
-    var title = u(".title-input").attr("value");
-    var author = u(".author-input").attr("value");
-    var subject = u(".subject-input").attr("value");
-    console.log(title, author, subject);
+    titleInput.value = "";
+    authorInput.value = "";
+    subjectInput.value = "";
+
     // Validate input
+    if (validateSearch(title, author, subject)) {
 
-    // Construct Google Books URL
+        // Construct Google Books URL
+        buildGoogleURL(title, author, subject);
 
-    // Call Google Books API
-
+        // Call Google Books API
+        callGoogleBooksAPI(googleURL);
+    }
 }
 
 // Google Books API callback function
 
-    // Extract results from the API response
+// Extract results from the API response
 
-    // Change document.location property to open search results page
+// Change document.location property to open search results page
 
 // Close button event listener
 u(".close-search").on("click", closeSearch);
