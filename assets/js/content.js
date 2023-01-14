8255225c66955d1cb97b76f77e421f46ce6309c2
Fayven <fayven.amelga@gmail.com>
2023-01-14 17:38:17 -0500

    Add nytApi eventlistener and var

---
+++ assets/js/content.js
@@ -0,0 +1,63 @@
+
+// what: click search result
+// how: eventlistener click, call on function to fetch api
+// why: generate url for NYT; call on fetch function
+
+// what: fetch nyt api
+// how: fetch function
+// why: to start process of calling on on api
+
+// what: store url
+// how: define var
+// why: to have a reference
+
+// what: var query selectors 
+// how: define var
+// why: to know where to link on html
+
+// what: call on nyt api
+// how: fetch
+// why: to contact the api for info
+
+// what: get the info
+// how: method get
+// why: get the info
+
+// what: return response
+// how: .then(function(response))
+// why: bring it back to js
+
+// what: see response
+// how: console.log(response)
+// why: for data to show up in console
+
+// what: post response to html
+// how: htmlquery.textContent or innerHTML(data.____)
+// why: to link response from console to html
+
+var chosenResult = "add from js";
+
+function nytApi(event) {
+    var requestUrl = "https://api.nytimes.com/svc/books/v3/reviews.json";
+    var requestUrl = "https://api.nytimes.com/svc/books/v3/reviews.json"
+    var author = "author=";
+    var title = "title=";
+    var authKey = "UI3A4nKlKvMGNvUFyFzz9QuLZzBX0JXq";
+
+    
+
+}
+
+chosenResult.handle('click', function(nytApi));
+
+// what: 
+// how:
+// why:
+
+// what:
+// how:
+// why:
+
+// what:
+// how:
+// why:
---
+++ assets/js/searchpage.js
@@ -0,0 +1,9 @@
+var titleTextEl = u('#title')
+var authorTextEl = u('#author')
+var searchResultsEl = u('#searchQs')
+var searchFormEl = u('#searchForm')
+var searchBtnEl = u('#searchBtn')
+
+function getSearch() {
+    var searchElArr = u('searc')
+}
